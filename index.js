ccc6c6be0005cd2ea2ab209273e01a952d59f100
myrteza <myrteza@hotmail.fr>
2023-10-07 16:38:10 +0200

    project done

---
+++ index.js
@@ -0,0 +1,5 @@
+var firstName = "Jhon";
+var lastName = "Doe";
+var multipleLineStr = "This is a multiple \nline string \nsupported by \ntypescript";
+var profile = "Hello i am ".concat(firstName, " ").concat(lastName, ", \nand i am a programmer");
+console.log(profile);
---
+++ index.ts
@@ -0,0 +1,13 @@
+let firstName : string = "Jhon"; 
+let lastName : string = "Doe";
+
+let multipleLineStr : string = `This is a multiple 
+line string 
+supported by 
+typescript`; 
+
+let profile : string = `Hello i am ${firstName} ${lastName}, 
+and i am a programmer`; 
+
+console.log(profile); 
+
